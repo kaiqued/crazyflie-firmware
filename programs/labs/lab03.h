01accef72cf3aec60f325481e028a4637fa99191
Kaiqued <kaiqued@al.insper.edu.br>
2021-08-30 11:53:28 -0300

    + Alterações pré lab

--- programs/labs/lab03.h
+++ programs/labs/lab03.h
@@ -5,22 +5,28 @@
             |**** DEFINIÇÕES DE PINOS ****|
             \*****************************/
 
-// Define all motors as PWM output objects
+// Define os motores como PWMOut
 PwmOut Mot1(MOTOR1);
 PwmOut Mot2(MOTOR2);
 PwmOut Mot3(MOTOR3);
 PwmOut Mot4(MOTOR4);
 
+// Define os Led's como DigitalOut
+DigitalOut Led_G_L(LED_GREEN_L,!false);
+DigitalOut Led_G_R(LED_GREEN_R,!false);
+DigitalOut Led_R_L(LED_RED_L,!false);
+DigitalOut Led_R_R(LED_RED_R,!false);
 
-            /*******************************\
-            |**** DEFINIÇÕES DE FUNÇÕES ****|
-            \*******************************/
 
-// Converts desired angular velocity (rad/s) to PWM signal (%)
-float control_motor(float OMEGA_R)
-{
-    return Motor_a2 * OMEGA_R * OMEGA_R + Motor_a1 * OMEGA_R;
-}
+            /**********************************\
+            |**** INICIALIZAÇÃO DE FUNÇÕES ****|
+            \**********************************/
+
+// Inicializa o Drone
+void INIT_DRONE();
+
+// Converte Velocidade angular (rad/s) para sinal PWM (%)
+float Rad2PWM(float OMEGA_R);
 
 
             /******************\
@@ -28,21 +34,42 @@ float control_motor(float OMEGA_R)
             \******************/
 
 int main()
-{   
-    // Set PWM frequency to 500Hz
-    Mot1.period(1.0/500.0);
-    Mot2.period(1.0/500.0);
-    Mot3.period(1.0/500.0);
-    Mot4.period(1.0/500.0);
+{
+    INIT_DRONE();
 
-    // Calcula o valor de PWM para tal rotação
-    float PWMCalculado = control_motor(1000);
+    float PWMCalculado = Rad2PWM(1000);
 
 
 
+
+    // Turn off red LEDs indicating motors are armed
+    Led_R_L = 1;
+    Led_R_R = 1;
     while(true)
     {
     }
     
 }
 
+
+            /********************************\
+            |**** DEFINIÇÕES DAS FUNÇÕES ****|
+            \********************************/
+
+void INIT_DRONE()
+{
+    // Set PWM frequency to 500Hz
+    Mot1.period(1.0/500.0);
+    Mot2.period(1.0/500.0);
+    Mot3.period(1.0/500.0);
+    Mot4.period(1.0/500.0);
+
+    // Turn on red LEDs indicating motors are armed
+    Led_R_L = 0;
+    Led_R_R = 0;
+}
+
+float Rad2PWM(float OMEGA)
+{
+    return Motor_a2 * OMEGA * OMEGA + Motor_a1 * OMEGA;
+}
